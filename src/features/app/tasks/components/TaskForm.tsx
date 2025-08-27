24a7279e09a65f3e5f36403950b9455ee664166d
Igor Nicoletti <igor93nicoletti@gmail.com>
2025-08-27 02:35:59 -0300

    TaskForm.tsx

--- src/features/app/tasks/components/TaskForm.tsx
+++ src/features/app/tasks/components/TaskForm.tsx
@@ -19,6 +19,7 @@ import {
   SelectValue
 } from '@/components/ui/select'
 import { Textarea } from '@/components/ui/textarea'
+import { ScrollArea } from '@/components/ui/scroll-area'
 import { labels, priorities, statuses } from '@/features/app/tasks/lib/schema'
 
 interface TaskFormProps<T extends FieldValues> extends Omit<ComponentPropsWithRef<'form'>, 'onSubmit'> {
@@ -33,131 +34,132 @@ export const TaskForm = <T extends FieldValues>({
   children
 }: TaskFormProps<T>) => (
   <Form {...form}>
-    <form onSubmit={form.handleSubmit(onSubmit)} className='flex flex-col flex-1 gap-6'>
-      <div className='grid auto-rows-min gap-4 px-4'>
-        <FormField
-          control={form.control}
-          name={'title' as FieldPath<T>}
-          render={({ field, fieldState }) => (
-            <FormItem>
-              <FormLabel>Title</FormLabel>
-              <FormControl>
-                <Textarea
-                  {...field}
-                  placeholder='Do a kickflip'
-                  className='resize-none'
-                  aria-invalid={!!fieldState.error} />
-              </FormControl>
-              <FormMessage className='text-xs text-right' />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name={'label' as FieldPath<T>}
-          render={({ field, fieldState }) => (
-            <FormItem>
-              <FormLabel>Label</FormLabel>
-              <Select onValueChange={field.onChange} value={field.value ?? ''}>
+    <form onSubmit={form.handleSubmit(onSubmit)} className='flex flex-col flex-1'>
+      <ScrollArea className='flex-1 px-4 py-2'>
+        <div className='grid auto-rows-min gap-4'>
+          <FormField
+            control={form.control}
+            name={'title' as FieldPath<T>}
+            render={({ field, fieldState }) => (
+              <FormItem>
+                <FormLabel>Title</FormLabel>
                 <FormControl>
-                  <SelectTrigger className='w-full capitalize'>
-                    <SelectValue
-                      placeholder='Select a label'
-                      aria-invalid={!!fieldState.error} />
-                  </SelectTrigger>
+                  <Textarea
+                    {...field}
+                    placeholder='Do a kickflip'
+                    className='resize-none'
+                    aria-invalid={!!fieldState.error} />
                 </FormControl>
-                <SelectContent>
-                  <SelectGroup>
-                    {labels.map((item) => (
-                      <SelectItem key={item} value={item} className='capitalize'>
-                        {item}
-                      </SelectItem>
-                    ))}
-                  </SelectGroup>
-                </SelectContent>
-              </Select>
-              <FormMessage className='text-xs text-right' />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name={'status' as FieldPath<T>}
-          render={({ field, fieldState }) => (
-            <FormItem>
-              <FormLabel>Status</FormLabel>
-              <Select onValueChange={field.onChange} value={field.value ?? ''}>
+                <FormMessage className='text-xs text-right' />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name={'label' as FieldPath<T>}
+            render={({ field, fieldState }) => (
+              <FormItem>
+                <FormLabel>Label</FormLabel>
+                <Select onValueChange={field.onChange} value={field.value ?? ''}>
+                  <FormControl>
+                    <SelectTrigger className='w-full capitalize'>
+                      <SelectValue
+                        placeholder='Select a label'
+                        aria-invalid={!!fieldState.error} />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    <SelectGroup>
+                      {labels.map((item) => (
+                        <SelectItem key={item} value={item} className='capitalize'>
+                          {item}
+                        </SelectItem>
+                      ))}
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+                <FormMessage className='text-xs text-right' />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name={'status' as FieldPath<T>}
+            render={({ field, fieldState }) => (
+              <FormItem>
+                <FormLabel>Status</FormLabel>
+                <Select onValueChange={field.onChange} value={field.value ?? ''}>
+                  <FormControl>
+                    <SelectTrigger className='w-full capitalize'>
+                      <SelectValue
+                        placeholder='Select a status'
+                        aria-invalid={!!fieldState.error} />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    <SelectGroup>
+                      {statuses.map((item) => (
+                        <SelectItem key={item} value={item} className='capitalize'>
+                          {item}
+                        </SelectItem>
+                      ))}
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+                <FormMessage className='text-xs text-right' />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name={'priority' as FieldPath<T>}
+            render={({ field, fieldState }) => (
+              <FormItem>
+                <FormLabel>Priority</FormLabel>
+                <Select onValueChange={field.onChange} value={field.value ?? ''}>
+                  <FormControl>
+                    <SelectTrigger className='w-full capitalize'>
+                      <SelectValue
+                        placeholder='Select a priority'
+                        aria-invalid={!!fieldState.error} />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    <SelectGroup>
+                      {priorities.map((item) => (
+                        <SelectItem key={item} value={item} className='capitalize'>
+                          {item}
+                        </SelectItem>
+                      ))}
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+                <FormMessage className='text-xs text-right' />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name={'estimatedHours' as FieldPath<T>}
+            render={({ field, fieldState }) => (
+              <FormItem>
+                <FormLabel>Estimated Hours</FormLabel>
                 <FormControl>
-                  <SelectTrigger className='w-full capitalize'>
-                    <SelectValue
-                      placeholder='Select a status'
-                      aria-invalid={!!fieldState.error} />
-                  </SelectTrigger>
+                  <Input
+                    {...field}
+                    type='number'
+                    placeholder='Enter estimated hours'
+                    step='0.5'
+                    min='0'
+                    value={field.value ?? ''}
+                    onChange={(event) => field.onChange(event.target.valueAsNumber)}
+                    aria-invalid={!!fieldState.error} />
                 </FormControl>
-                <SelectContent>
-                  <SelectGroup>
-                    {statuses.map((item) => (
-                      <SelectItem key={item} value={item} className='capitalize'>
-                        {item}
-                      </SelectItem>
-                    ))}
-                  </SelectGroup>
-                </SelectContent>
-              </Select>
-              <FormMessage className='text-xs text-right' />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name={'priority' as FieldPath<T>}
-          render={({ field, fieldState }) => (
-            <FormItem>
-              <FormLabel>Priority</FormLabel>
-              <Select onValueChange={field.onChange} value={field.value ?? ''}>
-                <FormControl>
-                  <SelectTrigger className='w-full capitalize'>
-                    <SelectValue
-                      placeholder='Select a priority'
-                      aria-invalid={!!fieldState.error} />
-                  </SelectTrigger>
-                </FormControl>
-                <SelectContent>
-                  <SelectGroup>
-                    {priorities.map((item) => (
-                      <SelectItem key={item} value={item} className='capitalize'>
-                        {item}
-                      </SelectItem>
-                    ))}
-                  </SelectGroup>
-                </SelectContent>
-              </Select>
-              <FormMessage className='text-xs text-right' />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name={'estimatedHours' as FieldPath<T>}
-          render={({ field, fieldState }) => (
-            <FormItem>
-              <FormLabel>Estimated Hours</FormLabel>
-              <FormControl>
-                <Input
-                  {...field}
-                  type='number'
-                  placeholder='Enter estimated hours'
-                  step='0.5'
-                  min='0'
-                  value={field.value ?? ''}
-                  onChange={(event) => field.onChange(event.target.valueAsNumber)}
-                  aria-invalid={!!fieldState.error} />
-              </FormControl>
-              <FormMessage className='text-xs text-right' />
-            </FormItem>
-          )}
-        />
-      </div>
+                <FormMessage className='text-xs text-right' />
+              </FormItem>
+            )}
+          />
+        </div>
       {children}
     </form>
   </Form>
