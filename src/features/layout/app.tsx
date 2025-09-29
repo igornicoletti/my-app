207e83893c96601967c7bb82f2649ef3cf58221f
igornicoletti <igor93nicoletti@gmail.com>
2025-09-29 02:43:24 -0300

    refactor: simplify dashboard and app layout components

--- src/features/layout/app.tsx
+++ src/features/layout/app.tsx
@@ -1,35 +1,14 @@
 import { CommonBreadcrumb } from '@/components/common/breadcrumb'
-import { SidebarNavigation } from '@/components/sidebar/navigation'
-import { SidebarTeam } from '@/components/sidebar/team'
-import { SidebarUser } from '@/components/sidebar/user'
+import { SidebarApp } from '@/components/sidebar/sidebar'
 import { Button } from '@/components/ui/button'
 import { Separator } from '@/components/ui/separator'
-import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarInset, SidebarProvider, SidebarRail, SidebarTrigger } from '@/components/ui/sidebar'
+import { SidebarInset, SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
 import { useAuth } from '@/providers/auth'
 import { useCommand } from '@/providers/command'
 import { useTheme } from '@/providers/theme'
-import { routeNavigations } from '@/routes/config/navigation'
 import { ServiceAuth } from '@/services/auth'
-import { CodaLogoIcon, LastfmLogoIcon, MagnifyingGlassIcon, MoonIcon, NyTimesLogoIcon, SignOutIcon, SunIcon } from '@phosphor-icons/react'
+import { MagnifyingGlassIcon, MoonIcon, SignOutIcon, SunIcon } from '@phosphor-icons/react'
 import { Navigate, Outlet } from 'react-router-dom'
-
-const teams = [
-  {
-    name: 'Acme Inc',
-    logo: NyTimesLogoIcon,
-    plan: 'Enterprise',
-  },
-  {
-    name: 'Acme Corp.',
-    logo: CodaLogoIcon,
-    plan: 'Startup',
-  },
-  {
-    name: 'Evil Corp.',
-    logo: LastfmLogoIcon,
-    plan: 'Free',
-  },
-]
 
 export const LayoutApp = () => {
   const { user } = useAuth()
@@ -42,19 +21,7 @@ export const LayoutApp = () => {
 
   return (
     <SidebarProvider>
-      <Sidebar collapsible='icon'>
-        <SidebarHeader>
-          <SidebarTeam teams={teams} />
-        </SidebarHeader>
-        <SidebarContent>
-          {routeNavigations.map((group) => (
-            <SidebarNavigation key={group.label} label={group.label} items={group.items} />))}
-        </SidebarContent>
-        <SidebarFooter>
-          <SidebarUser user={user} />
-        </SidebarFooter>
-        <SidebarRail />
-      </Sidebar>
+      <SidebarApp collapsible='icon' user={user} />
       <SidebarInset>
         <header className='flex h-16 shrink-0 items-center gap-2 p-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12'>
           <div className='flex flex-1 items-center gap-2 p-2'>
